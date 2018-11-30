65d0a6389baeaaa4c5195dc721c9fb6bba767942
henry halkano <halkanoh10@gmail.com>
2018-11-30 16:41:45 +0300

    added some values to my js page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 var triangleTracker = function(){
-  
+  var first = document.getElementById("one").value;
+
+var second = document.getElementById("two").value;
+
+var third = document.getElementById("three").value;
 };
