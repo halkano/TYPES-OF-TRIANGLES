0029c4c6293b11b8eb288be7379b7c0ba8c6d9df
henry halkano <halkanoh10@gmail.com>
2018-11-30 16:52:31 +0300

    added a scalene condition to my js page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var triangleTracker = function(){
-  var first = document.getElementById("one").value;
+            var first = document.getElementById("one").value;
 
-var second = document.getElementById("two").value;
+            var second = document.getElementById("two").value;
 
-var third = document.getElementById("three").value;
+            var third = document.getElementById("three").value;
 
 
 if (first === second && second === third && first === third){
@@ -13,5 +13,12 @@ alert("Equilateral Triangle");
   else if(first === second || second === third || first === third){
 
 alert("Isosceles Triangle");
+  }
+  else if((first + second) <= (third) || (first +third) <= (second) (second + third) <= (first)){
+
+alert("Scalene Triangle");
+  }
+else {
+    alert("NOT a triangle");
   }
 };
